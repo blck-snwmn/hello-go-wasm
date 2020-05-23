56a32533a46addbce6ec512b6252f493d1c021b1
blck-snwmn <whitesnowmancreator@gmail.com>
2020-05-24 01:36:41 +0900

    Add svg sample

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"syscall/js"
 )
 
@@ -24,5 +25,47 @@ func main() {
 		nameInput.Set("value", nil)
 		return nil
 	}))
+	drawSvg(20, 30, 50, 50)
 	select {}
+}
+
+func drawSvg(iniX, iniY, iniWidth, iniHeight int) {
+	document := js.Global().Get("document")
+	target := document.Call("getElementById", "target")
+	chX := registerChangeEvent("input-x")
+	chY := registerChangeEvent("input-y")
+	chW := registerChangeEvent("input-width")
+	chH := registerChangeEvent("input-height")
+
+	oldX, oldY, oldW, oldH := iniX, iniY, iniWidth, iniHeight
+	go func() {
+		for {
+			target.Call("setAttribute", "x", oldX)
+			target.Call("setAttribute", "y", oldY)
+			target.Call("setAttribute", "width", oldW)
+			target.Call("setAttribute", "height", oldH)
+			select {
+			case oldX = <-chX:
+			case oldY = <-chY:
+			case oldW = <-chW:
+			case oldH = <-chH:
+			}
+		}
+	}()
+}
+
+func registerChangeEvent(elementID string) <-chan int {
+	ch := make(chan int, 0)
+	document := js.Global().Get("document")
+	input := document.Call("getElementById", elementID)
+	input.Call("addEventListener", "change", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		input := this.Get("value").String()
+		fmt.Println(input)
+		v, err := strconv.Atoi(input)
+		if err == nil { //if NO error, send value.
+			ch <- v
+		}
+		return nil
+	}))
+	return ch
 }
